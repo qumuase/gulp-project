f6bfd362278beb3a6ae17ced305ecb5e77039911
王志鸣 <986168275@qq.com>
2016-12-26 22:24:43 +0800

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp'),
 	gulpSequence = require('gulp-sequence'),
 	sass = require('gulp-sass'),
+	cleanCss = require('gulp-clean-css'),
 	htmlPrettify = require('gulp-html-prettify'),
 	browserSync = require('browser-sync');
 
@@ -15,9 +16,20 @@ gulp.task('html',function(){
 
 gulp.task('sass',function(){
 	gulp.src('./src/**/*.scss')
-	.pipe(sass())
+	.pipe(sass({outputStyle: 'expanded'}))
 	.pipe(gulp.dest('./dist/asset'));
 });
+
+gulp.task('cssmin',function(){
+	gulp.src('./dist/**/*.css')
+		.pipe(cleanCss(){
+			adcanced: false,
+			compatibility: 'ie8',
+			keepBreaks: true,
+			keepSpecialComments: '*'
+		})
+		.pipe(gulp.dest('./dist/asset'));
+})
 
 gulp.task('watch',function(){
 	gulp.watch('./src/**/*.html',['html']);
